574d7767ab3e80cdc705d0ac1a439c6979af9dfe
cmeng0321 <1640291556@qq.com>
2019-04-05 14:34:14 +0800

    first commit code

---
+++ tools.h
@@ -0,0 +1,9 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+void SetWindowSize(int cols, int lines);
+void SetColor(int colorID);
+void SetCursorPosition(const int x, const int y);
+void SetBackColor();
+
+#endif
